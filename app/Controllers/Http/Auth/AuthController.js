0ff76e670da697655243238ccd42ba81d7086e2f
fernandoors <fernando.ors@outlook.com>
2021-02-20 18:54:02 -0300

    ADD AUTH METHODS TO REGISTER AND LOG-IN/OUT

--- app/Controllers/Http/Auth/AuthController.js
+++ app/Controllers/Http/Auth/AuthController.js
@@ -1,10 +1,16 @@
 'use strict'
-'use strict'
 
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
+const Database = use('Database')
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const User = use('App/Models/User')
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Role = use('Role')
 class AuthController {
   /**
    * @param {object} ctx
@@ -12,6 +18,21 @@ class AuthController {
    * @param {Response} ctx.response
    */
   async register({ request, response }) {
+    const trx = await Database.beginTransaction()
+    try {
+      const { email, password, name, surname } = request.all()
+      const user = await User.create({ email, password, name, surname }, trx)
+      const userRole = await Role.findBy('slug', 'customer')
+      await user.roles().attach([userRole.id], null, trx)
+
+      await trx.commit()
+      return response.status(201).send({ data: user })
+    } catch (error) {
+      await trx.rollback()
+      return response.status(400).send({
+        message: 'Erro ao realizar cadastro!'
+      })
+    }
   }
 
   /**
@@ -19,21 +40,40 @@ class AuthController {
      * @param {Request} ctx.request
      * @param {Response} ctx.response
      */
-  async login({ request, response, auth }) { }
+  async login({ request, response, auth }) {
+    const { email, password } = request.all()
+    const data = await auth.withRefreshToken().attempt(email, password)
+    return response.send({ data })
+  }
 
   /**
      * @param {object} ctx
      * @param {Request} ctx.request
      * @param {Response} ctx.response
      */
-  async refresh({ request, response, auth }) { }
+  async refresh({ request, response, auth }) {
+    let refresh_token = request.input('refresh_token')
+    if (!refresh_token) {
+      refresh_token = request.header('refresh_token')
+    }
+    const user = await auth.newRefreshToken().generateForRefreshToken(refresh_token)
+
+    return response.send({ data: user })
+  }
 
   /**
      * @param {object} ctx
      * @param {Request} ctx.request
      * @param {Response} ctx.response
      */
-  async logout({ request, response, auth }) { }
+  async logout({ request, response, auth }) {
+    let refresh_token = request.input('refresh_token')
+    if (!refresh_token) {
+      refresh_token = request.header('refresh_token')
+    }
+    await auth.authenticator('jwt').revokeTokens([refresh_token], true)
+    return response.status(204)
+  }
 
   /**
      * @param {object} ctx
