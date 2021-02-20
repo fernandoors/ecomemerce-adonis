f84e43a5d6a25dfa3a097e76a8b9511c5c419160
fernandoors <fernando.ors@outlook.com>
2021-02-20 18:21:36 -0300

    ADD ADMIN ROUTES AND CONTROLLERS

--- app/Controllers/Http/Auth/AuthController.js
+++ app/Controllers/Http/Auth/AuthController.js
@@ -1,18 +1,59 @@
 'use strict'
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
 
 class AuthController {
-  async register({ request, response }) {}
+  /**
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async register({ request, response }) {
+  }
 
+  /**
+     * @param {object} ctx
+     * @param {Request} ctx.request
+     * @param {Response} ctx.response
+     */
   async login({ request, response, auth }) { }
 
+  /**
+     * @param {object} ctx
+     * @param {Request} ctx.request
+     * @param {Response} ctx.response
+     */
   async refresh({ request, response, auth }) { }
 
+  /**
+     * @param {object} ctx
+     * @param {Request} ctx.request
+     * @param {Response} ctx.response
+     */
   async logout({ request, response, auth }) { }
 
+  /**
+     * @param {object} ctx
+     * @param {Request} ctx.request
+     * @param {Response} ctx.response
+     */
   async forgot({ request, response }) { }
 
+  /**
+     * @param {object} ctx
+     * @param {Request} ctx.request
+     * @param {Response} ctx.response
+     */
   async remember({ request, response }) { }
 
+  /**
+     * @param {object} ctx
+     * @param {Request} ctx.request
+     * @param {Response} ctx.response
+     */
   async reset({ request, response }) { }
 }
 
