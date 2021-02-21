4d03dac52323dc85ce7cff11b74b085b9fdc33b8
fernandoors <fernando.ors@outlook.com>
2021-02-21 19:11:49 -0300

    ADD PRODUCT CRUD

--- app/Controllers/Http/Admin/ProductController.js
+++ app/Controllers/Http/Admin/ProductController.js
@@ -7,6 +7,10 @@
 /**
  * Resourceful controller for interacting with products
  */
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Product = use('App/Models/Product')
+
 class ProductController {
   /**
    * Show a list of all products.
@@ -17,19 +21,14 @@ class ProductController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
-  }
-
-  /**
-   * Render a form to be used for creating a new product.
-   * GET products/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
+  async index({ request, response, pagination }) {
+    const name = request.input('name')
+    const query = Product.query()
+    if (!!name) {
+      query.where('name', 'LIKE', `%${name}%`)
+    }
+    const products = await query.paginate(pagination.page, pagination.limit)
+    return response.send(products)
   }
 
   /**
@@ -40,7 +39,16 @@ class ProductController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store ({ request, response }) {
+  async store({ request, response }) {
+    try {
+      const { name, description, price, image_id } = request.all()
+      const product = await Product.create({ name, description, price, image_id })
+      return response.status(201).send(product)
+    } catch (error) {
+      return response.status(400).send({
+        message: 'Erro ao processar a sua solicitação!'
+      })
+    }
   }
 
   /**
@@ -52,19 +60,15 @@ class ProductController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
-  }
-
-  /**
-   * Render a form to update an existing product.
-   * GET products/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
+  async show({ params: { id }, response }) {
+    try {
+      const product = await Product.findOrFail(id)
+      return response.send(product)
+    } catch (error) {
+      return response.status(500).send({
+        message: 'Erro ao processar a sua solicitação!'
+      })
+    }
   }
 
   /**
@@ -75,7 +79,18 @@ class ProductController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
+  async update({ params: { id }, request, response }) {
+    try {
+      const product = await Product.findOrFail(id)
+      const { name, description, price, image_id } = request.all()
+      product.merge({ name, description, price, image_id })
+      await product.save()
+      return response.send(product)
+    } catch (error) {
+      return response.status(400).send({
+        message: 'Erro ao processar a sua solicitação!'
+      })
+    }
   }
 
   /**
@@ -86,7 +101,16 @@ class ProductController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy({ params: { id }, response }) {
+    try {
+      const product = await Product.findOrFail(id)
+      await product.delete()
+      return response.status(204).send({})
+    } catch (error) {
+      return response.status(500).send({
+        message: 'Erro ao processar a sua solicitação!'
+      })
+    }
   }
 }
 
