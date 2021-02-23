9b300e03ba214ba20e090d6065d46d0418c8dcb2
fernandoors <fernando.ors@outlook.com>
2021-02-23 11:21:24 -0300

    ADD ADMIN MIDDLEWARE

--- start/routes/admin.js
+++ start/routes/admin.js
@@ -13,4 +13,4 @@ Route.group(() => {
   Route.resource('orders', 'OrderController').apiOnly()
   Route.resource('images', 'ImageController').apiOnly()
   Route.resource('users', 'UserController').apiOnly()
-}).prefix('v1/admin').namespace('Admin')
+}).prefix('v1/admin').namespace('Admin').middleware(['auth', 'is:( admin || manager )'])
