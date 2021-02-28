18af61447473dd6a39d49107ff03757ad992594d
fernandoors <fernando.ors@outlook.com>
2021-02-27 21:52:32 -0300

    ADD ORDER CONTROLLER CRUD PLUS DISCOUNTS

--- app/Controllers/Http/Client/OrderController.js
+++ app/Controllers/Http/Client/OrderController.js
@@ -7,6 +7,17 @@
 /**
  * Resourceful controller for interacting with orders
  */
+const Database = use('Database')
+const Ws = use('Ws')
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Order = use('App/Models/Order')
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Coupon = use('App/Models/Coupon')
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Discount = use('App/Models/Discount')
+const Transformer = use('App/Transformers/Admin/OrderTransformer')
+const Service = use('App/Service/Order/OrderService')
+
 class OrderController {
   /**
    * Show a list of all orders.
@@ -17,19 +28,17 @@ class OrderController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
-  }
-
-  /**
-   * Render a form to be used for creating a new order.
-   * GET orders/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
+  async index({ request, response, pagination, transform, auth }) {
+    const client = await auth.getUser()
+    const { number } = request.only(['number'])
+    const query = Order.query()
+    if (!!number) {
+      query.where('id', 'LIKE', `%${number}%`)
+    }
+    query.where('user_id', client.id)
+    let orders = await query.orderBy('id', 'DESC').paginate(pagination.page, pagination.limit)
+    orders = await transform.paginate(orders, Transformer)
+    return response.send(orders)
   }
 
   /**
@@ -40,7 +49,30 @@ class OrderController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store ({ request, response }) {
+  async store({ request, response, auth, transform }) {
+    const trx = await Database.beginTransaction()
+    try {
+      const items = request.input('items')
+      const client = await auth.getUser()
+      let order = await Order.create({ user_id: client.id, trx })
+      const service = new Service(order, trx)
+      if (!!items.length) {
+        await service.syncItems(items)
+      }
+      await trx.commit()
+      order = await Order.find(order.id)
+      order = await transform.includes('items').item(order, Transformer)
+      const topic = Ws.getChannel('notifications').topic('notifications')
+      if (topic) {
+        topic.broadcast('new:order', order)
+      }
+      return response.status(201).send(order)
+    } catch (error) {
+      await trx.rollback()
+      return response.status(400).send({
+        message: 'Erro ao processar a sua solicitação!'
+      })
+    }
   }
 
   /**
@@ -52,41 +84,86 @@ class OrderController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
+  async show({ params: { id }, response, transform }) {
+    const client = await auth.getUser()
+    const result = await Order.query().where('user_id', client.id).where('id', id).firstOrFail()
+    const order = await transform.item(result, Transformer)
+    return response.send(order)
   }
 
   /**
-   * Render a form to update an existing order.
-   * GET orders/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
-  }
-
-  /**
-   * Update order details.
-   * PUT or PATCH orders/:id
-   *
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
-  }
+  async update({ params: { id }, request, auth, response }) {
+    const client = await auth.getUser()
+    let order = await Order.query().where('user_id', client.id).where('id', id).firstOrFail()
+    const trx = Database.beginTransaction()
+    try {
+      const { items, status } = request.all()
+      order.merge({ user_id: client.id, status })
+      const service = new Service(order, trx)
+      await service.updateItems(items)
+      await order.save(trx)
+      await trx.commit()
+      order = await transform.includes('items,coupons,discounts').item(order, Transformer)
+      return response.send(order)
+    } catch (error) {
+      await trx.rollback()
+      return response.status(400).send({
+        message: 'Erro ao processar a sua solicitação!'
+      })
+    }
 
+  }
   /**
-   * Delete a order with id.
-   * DELETE orders/:id
-   *
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async applyDiscount({ params: { id }, request, response, transform, auth }) {
+    const { code } = request.all()
+    const coupon = await Coupon.findByOrFail('code', code.toUpperCase())
+    const client = await auth.getUser()
+    let order = await Order.query().where('user_id', client.id).where('id', id).firstOrFail()
+    let info = {}
+    try {
+      const service = new Service(order)
+      const canAddDiscount = await service.canApplyDiscount(coupon)
+      const orderDiscounts = await order.coupons().getCount()
+      const canApplyToOrder = orderDiscounts < 1 || (!!orderDiscounts && coupon.recursive)
+      if (canAddDiscount && canApplyToOrder) {
+        await Discount.findOrCreate({
+          order_id: order.id,
+          coupon_id: coupon.id,
+        })
+        info.message = 'Cupom aplicado com sucesso!'
+        info.success = true
+      } else {
+        info.message = 'Erro ao aplicar o cupom!'
+        info.success = false
+      }
+      order = await transform.include('coupons,items,discounts').item(order, Transformer)
+      return response.send({ order, info })
+    } catch (error) {
+      return response.status(500).send({
+        message: 'Erro ao processar a sua solicitação!'
+      })
+    }
+  }
+  /**
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async removeDiscount({ params: { id }, request, response, auth }) {
+    const client = await auth.getUser()
+    const { discount_id } = request.all()
+    await Order.query().where('user_id', client.id).where('id', id).firstOrFail()
+    const discount = await Discount.findOrFail(discount_id)
+    await discount.delete()
+    return response.status(204).send({})
   }
 }
 
--- start/routes/client.js
+++ start/routes/client.js
@@ -8,9 +8,11 @@ Route.group(() => {
   Route.get('products', 'ProductController.index')
   Route.get('products/:id', 'ProductController.show')
 
-  Route.get('orders', 'OrderController.get')
-  Route.get('orders/:id', 'OrderController.show')
-  Route.post('orders', 'OrderController.store')
-  Route.put('orders/:id', 'OrderController.put')
+  Route.get('orders', 'OrderController.get').middleware(['auth'])
+  Route.get('orders/:id', 'OrderController.show').middleware(['auth'])
+  Route.post('orders', 'OrderController.store').middleware(['auth'])
+  Route.put('orders/:id', 'OrderController.put').middleware(['auth'])
+  Route.put('orders/:id/discount', 'OrderController.applyDiscount').middleware(['auth'])
+  Route.delete('orders/:id', 'OrderController.removeDiscount').middleware(['auth'])
 
 }).prefix('v1').namespace('Client')
